2f8005942521b7c892a9f225395eeeec385933e8
Rauli Laine <rauli.laine@iki.fi>
2024-09-20 15:41:07 +0300

    WIP: Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Varasto server C++ implementation
+
+C++17 implementation of [Varasto server]. Work in progress.
+
+[Varasto server]: https://www.npmjs.com/package/@varasto/server
+
+## TODO
+
+- Caching.
+- SSL support.
+- Basic authentication support.
---
+++ src/filesystem-storage.cpp
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2024, Rauli Laine
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#include <fstream>
+
+#include "./filesystem-storage.hpp"
+#include "./slug.hpp"
+
+namespace varasto
+{
+  FilesystemStorage::FilesystemStorage(const path_type& root)
+    : m_root(root) {}
+
+  Storage::get_result_type
+  FilesystemStorage::Get(
+    const key_type& ns,
+    const key_type& key
+  ) const
+  {
+    const auto entry_and_path_result = GetEntryAndPath(ns, key);
+
+    if (entry_and_path_result)
+    {
+      return get_result_type::ok(entry_and_path_result.value().second);
+    }
+
+    return get_result_type::error(entry_and_path_result.error());
+  }
+
+  Storage::get_all_keys_type
+  FilesystemStorage::GetAllKeys(
+    const key_type& ns
+  ) const
+  {
+    if (is_valid_slug(ns))
+    {
+      const auto ns_path = m_root / ns;
+      std::vector<key_type> keys;
+
+      if (std::filesystem::is_directory(ns_path))
+      {
+        for (const auto& entry : std::filesystem::directory_iterator(ns_path))
+        {
+          if (std::filesystem::is_regular_file(entry))
+          {
+            keys.push_back(entry.path().filename().string());
+          }
+        }
+      }
+
+      return get_all_keys_type::ok(keys);
+    }
+
+    return get_all_keys_type::error("Invalid namespace: " + ns);
+  }
+
+  Storage::set_result_type
+  FilesystemStorage::Set(
+    const key_type& ns,
+    const key_type& key,
+    const value_type& value
+  )
+  {
+    const auto path_result = GetPath(ns, key);
+
+    if (path_result)
+    {
+      const auto ns_path = m_root / ns;
+
+      if (!std::filesystem::is_directory(ns_path))
+      {
+        if (!std::filesystem::create_directories(ns_path))
+        {
+          return set_result_type::error(
+            "Failed to create namespace directory."
+          );
+        }
+      }
+
+      {
+        std::ofstream file(path_result.value());
+
+        if (!file.good())
+        {
+          return set_result_type::error("Failed to open file.");
+        }
+        file << value;
+        file.close();
+      }
+
+      return set_result_type::ok(true);
+    }
+
+    return set_result_type::error(path_result.error());
+  }
+
+  Storage::delete_result_type
+  FilesystemStorage::Delete(
+    const key_type& ns,
+    const key_type& key
+  )
+  {
+    const auto entry_and_path_result = GetEntryAndPath(ns, key);
+
+    if (entry_and_path_result)
+    {
+      const auto& entry_and_path = entry_and_path_result.value();
+
+      if (std::filesystem::remove(entry_and_path.first))
+      {
+        // TODO: Delete orphan directories.
+        return delete_result_type::ok(entry_and_path.second);
+      }
+
+      return delete_result_type::ok(std::nullopt);
+    }
+
+    return delete_result_type::error(entry_and_path_result.error());
+  }
+
+  FilesystemStorage::get_path_result_type
+  FilesystemStorage::GetPath(
+    const key_type& ns,
+    const key_type& key
+  ) const
+  {
+    if (!is_valid_slug(ns))
+    {
+      return get_path_result_type::error("Invalid namespace: " + ns);
+    }
+    else if (!is_valid_slug(key))
+    {
+      return get_path_result_type::error("Invalid key: " + ns);
+    }
+
+    return get_path_result_type::ok(m_root / ns / key);
+  }
+
+  FilesystemStorage::get_entry_and_path_result_type
+  FilesystemStorage::GetEntryAndPath(
+    const key_type& ns,
+    const key_type& key
+  ) const
+  {
+    const auto path_result = GetPath(ns, key);
+
+    if (path_result)
+    {
+      const auto& path = path_result.value();
+
+      if (std::filesystem::is_regular_file(path))
+      {
+        std::ifstream file(path);
+
+        if (!file.good())
+        {
+          return get_entry_and_path_result_type::error("Failed to open file.");
+        }
+        try
+        {
+          const auto value = value_type::parse(file);
+
+          if (value.is_object())
+          {
+            return get_entry_and_path_result_type::ok(
+              std::make_pair(path, value)
+            );
+          }
+        }
+        catch (const std::exception& e)
+        {
+          return get_entry_and_path_result_type::error(e.what());
+        }
+      }
+
+      return get_entry_and_path_result_type::ok(
+        std::make_pair(path, std::nullopt)
+      );
+    }
+
+    return get_entry_and_path_result_type::error(path_result.error());
+  }
+}
